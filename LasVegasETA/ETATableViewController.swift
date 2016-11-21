2bf82dd8c85c51b0c2e0a6ba3172b6460a552ea8
Michele Cantelli <emmekappa@gmail.com>
2016-11-21 01:36:59 +0100

    Handling editing list.

--- LasVegasETA/ETATableViewController.swift
+++ LasVegasETA/ETATableViewController.swift
@@ -16,6 +16,8 @@ class ETATableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         loadETAs()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -24,7 +26,10 @@ class ETATableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true)  { timer in
             //self.loadETAs()
-            DispatchQueue.main.async{
+            if(self.tableView.isEditing) {
+                return
+            }
+            DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
@@ -83,25 +88,24 @@ class ETATableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            etas.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
